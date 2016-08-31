0000469b78d8edc96d2fafd2752823d20cff523d
graymi <michaelrgray1988@gmaillcom>
2016-08-30 21:34:42 -0700

    Set activation bool for yellow cards

--- MachiKoro/YellowCard.cpp
+++ MachiKoro/YellowCard.cpp
@@ -7,18 +7,24 @@ void YellowCard::action(Bank *p1, Bank *p2, Card *c1, Card *c2, int val)
 	// Nothing to really do here yet
 }
 
+TrainStation::TrainStation() {
+	this->active = false;
+	this->set_card("TrainStation", 4, 0, 0, 0, Icon::establishment);
+}
+
+ShoppingMall::ShoppingMall() {
+	this->active = false;
+	this->set_card("ShoppingMall", 10, 0, 0, 0, Icon::establishment);
+}
+
 AmusementPark::AmusementPark() {
+	this->active = false;
 	this->set_card("AmusementPark", 16, 0, 0, 0, Icon::establishment);
 }
 
 RadioTower::RadioTower() {
+	this->active = false;
 	this->set_card("RadioTower", 22, 0, 0, 0, Icon::establishment);
 }
 
-ShoppingMall::ShoppingMall() {
-	this->set_card("ShoppingMall", 10, 0, 0, 0, Icon::establishment);
-}
 
-TrainStation::TrainStation() {
-	this->set_card("TrainStation", 4, 0, 0, 0, Icon::establishment);
-}
--- MachiKoro/YellowCard.h
+++ MachiKoro/YellowCard.h
@@ -7,6 +7,7 @@ using namespace machi;
 class YellowCard : public Card {
 public:
 	virtual void action(Bank *p1, Bank *p2, Card *c1, Card *c2, int val);
+	bool active;
 private:
 };
 
